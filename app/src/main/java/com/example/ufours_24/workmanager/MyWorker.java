8947cf94239e0f9e73704401347d12d03cc15311
ufours-24 <raandroid91@gmail.com>
2019-01-18 12:27:01 +0530

    Notification

--- app/src/main/java/com/example/ufours_24/workmanager/MyWorker.java
+++ app/src/main/java/com/example/ufours_24/workmanager/MyWorker.java
@@ -1,7 +1,10 @@
 package com.example.ufours_24.workmanager;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import androidx.work.Result;
@@ -23,6 +26,26 @@ public class MyWorker extends Worker
     public Result doWork()
     {
         Log.d(TAG, "doWork called");
+        sendNotification("Simple Work Manager", "I have been send by WorkManager.");
+
         return Result.success();
     }
+
+    public void sendNotification(String title, String message) {
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //If on Oreo then notification required a notification channel.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("default", "Default", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), "default")
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        notificationManager.notify(1, notification.build());
+    }
+
 }
